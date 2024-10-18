136ef4134d025564a773689d9254dfb3f5538883
Ar1ocker <ar1oc@yandex.ru>
2024-10-18 19:03:51 +0300

    Using Prettier

--- commander-info.js
+++ commander-info.js
@@ -1,10 +1,8 @@
-import BasePlugin from './base-plugin.js';
+import BasePlugin from "./base-plugin.js";
 
 export default class CommandersInfo extends BasePlugin {
   static get description() {
-    return (
-        'Плагин выводящий броадкастом CMD сторон если они есть с двух сторон, выводит 1 раз через определенное время'
-    );
+    return "Плагин выводящий броадкастом CMD сторон если они есть с двух сторон, выводит 1 раз через определенное время";
   }
 
   static get defaultEnabled() {
@@ -15,19 +13,21 @@ export default class CommandersInfo extends BasePlugin {
     return {
       startSearchTimeout: {
         required: false,
-        description: 'Таймаут после начала матча, после которого идёт первая проверка на наличие командиров сторон за обе команды, в мс',
-        default: 10 * 60 * 1000
+        description:
+          "Таймаут после начала матча, после которого идёт первая проверка на наличие командиров сторон за обе команды, в мс",
+        default: 10 * 60 * 1000,
       },
       repeatSearchTimeout: {
         required: false,
-        description: 'Таймаут для повторных проверок, а появились ли командиры сторон за обе команды, в мс',
-        default: 5 * 60 * 1000
+        description:
+          "Таймаут для повторных проверок, а появились ли командиры сторон за обе команды, в мс",
+        default: 5 * 60 * 1000,
       },
       countMaxSearch: {
         required: false,
-        description: 'Количество проверок',
-        default: 3
-      }
+        description: "Количество проверок",
+        default: 3,
+      },
     };
   }
 
@@ -41,28 +41,40 @@ export default class CommandersInfo extends BasePlugin {
   }
 
   async searchCommanders() {
-    if (this.searchCommandersTimeout) clearTimeout(this.searchCommandersTimeout);
+    if (this.searchCommandersTimeout)
+      clearTimeout(this.searchCommandersTimeout);
     if (this.count_searches >= this.options.countMaxSearch) return;
 
     this.count_searches++;
-    
+
     let commanders = this.server.players.filter((player) => {
-      return player.isLeader && player.squad.squadName === 'Command Squad'
+      return player.isLeader && player.squad.squadName === "Command Squad";
     });
 
     if (commanders.length === 2 && commanders[0].name && commanders[1].name) {
-      await this.server.rcon.broadcast(`CMD сторон: ${commanders[0].name} и ${commanders[1].name}!`);
+      await this.server.rcon.broadcast(
+        `CMD сторон: ${commanders[0].name} и ${commanders[1].name}!`
+      );
       return;
     }
 
-    this.searchCommandersTimeout = setTimeout(this.searchCommanders, this.options.repeatSearchTimeout);
+    this.searchCommandersTimeout = setTimeout(
+      this.searchCommanders,
+      this.options.repeatSearchTimeout
+    );
   }
 
   async mount() {
-    this.server.on('NEW_GAME', () => {
+    this.server.on("NEW_GAME", () => {
       this.count_searches = 0;
-      if (this.searchCommandersTimeout) clearTimeout(this.searchCommandersTimeout);
-      this.searchCommandersTimeout = setTimeout(this.searchCommanders, this.options.startSearchTimeout);
-    })
+      if (this.searchCommandersTimeout) {
+        clearTimeout(this.searchCommandersTimeout);
+      }
+
+      this.searchCommandersTimeout = setTimeout(
+        this.searchCommanders,
+        this.options.startSearchTimeout
+      );
+    });
   }
 }
